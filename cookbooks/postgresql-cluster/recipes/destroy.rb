688307d05ef77de47ba1dcef50c2d45a950d8f8b
kulkarnisourabh <sourabh@kaodim.com>
2018-07-25 18:36:49 +0800

    Removed unnecessary cookbooks

--- cookbooks/postgresql-cluster/recipes/destroy.rb
+++
@@ -1,1 +0,0 @@
-include_recipe "postgresql-cluster::destroy_#{node['postgresql-cluster']['provisioning']['driver']}"
--- cookbooks/postgresql-cluster/recipes/destroy_vagrant.rb
+++
@@ -1,6 +0,0 @@
-include_recipe 'chef-provisioning-vagrant-helper::default'
-
-machine_batch do
-  action :destroy
-  machines search(:node, '*:*').map { |n| n.name }
-end
