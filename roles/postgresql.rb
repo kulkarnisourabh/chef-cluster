5100a89aa56150612b2fa4b168b96db05925f1b4
kulkarnisourabh <sourabh@kaodim.com>
2018-08-01 15:37:21 +0800

    added postgresql recepies in roles

---
+++ roles/postgresql.rb
@@ -0,0 +1,12 @@
+name        "postgresql"
+description "PostgreSQL Server Support"
+
+run_list(
+	"recipe[apt]",
+	"recipe[ssh_deploy_keys]",
+	"recipe[postgresql::repository]", 
+	"recipe[postgresql::access]", 
+	"recipe[postgresql::client_install]", 
+	"recipe[postgresql::server_install]",
+	"recipe[apt::unattended-upgrades]"
+	)    
