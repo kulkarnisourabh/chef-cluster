acbde07a86ade7aef30880f11eeec405cf986cf5
kulkarnisourabh <sourabh@kaodim.com>
2018-08-03 10:25:09 +0800

    divided postgresql.rb roles into master and slave

---
+++ roles/postgresql_slave.rb
@@ -0,0 +1,13 @@
+name        "postgresql_slave"
+description "PostgreSQL Server Support"
+
+run_list(
+	"recipe[apt]",
+	"recipe[ssh_deploy_keys]",
+	"recipe[postgresql::repository]", 
+	"recipe[postgresql::access]", 
+	"recipe[postgresql::client_install]", 
+	"recipe[postgresql::server_install]",
+	"recipe[apt::unattended-upgrades]",
+	"recipe[postgresql::backup_dir]"
+	)    
